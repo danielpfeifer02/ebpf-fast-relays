5d0f3684ccdfa70c99b0e092b2c2b044231ed8fd
danielpfeifer02 <pfeifer.daniel@gmx.at>
2024-04-18 17:17:19 +0200

    Relay only needs normal quic connections. Signaling server that it should start sending not yet working as clients request not fully reaches server (apparently?)

--- src/go/examples/priority_drop_video/video_transmission.go
+++ src/go/examples/priority_drop_video/video_transmission.go
@@ -8,17 +8,24 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
+	"fmt"
 	"log"
 	"math/big"
 	"os"
 	"time"
 
 	moqtransport "github.com/danielpfeifer02/priority-moqtransport"
+	"github.com/danielpfeifer02/quic-go-prio-packs"
 	"github.com/mengelbart/gst-go"
 )
 
 const video_server_address = "192.168.10.1:1909"
 const relay_server_address = "192.168.11.2:1909"
+
+type Client struct {
+	conn   quic.Connection
+	stream quic.Stream
+}
 
 func video_main(user string) {
 
@@ -82,98 +89,77 @@ func server() error {
 		TLSConfig: video_generateTLSConfig(),
 	}
 	if err := s.ListenQUIC(context.Background(), video_server_address); err != nil {
-		return err
+		panic(err)
 	}
 	return nil
 }
 
 func relay() error {
-	announcementCh := make(chan string)
-	closeCh := make(chan struct{})
 
-	c, err := moqtransport.DialQUIC(context.Background(), video_server_address)
+	fmt.Println("connecting to server")
+
+	server_conn, err := quic.DialAddr(context.Background(),
+		video_server_address, video_generateTLSConfig(), video_generateQuicConfig())
 	if err != nil {
-		return err
+		panic(err)
 	}
-
-	log.Println("moq peer connected")
-	c.OnAnnouncement(func(s string) error {
-		log.Printf("handling announcement of track %v", s)
-		announcementCh <- s
-		return nil
-	})
-
-	trackname := <-announcementCh
-	log.Printf("got announcement: %v", trackname)
-	p, err := gst.NewPipeline("appsrc name=src ! multipartdemux ! jpegdec ! autovideosink")
+	fmt.Println("connected to server")
+	server_str, err := server_conn.OpenStreamSync(context.Background())
 	if err != nil {
-		return err
+		panic(err)
 	}
-	t, err := c.Subscribe(trackname)
-	if err != nil {
-		return err
-	}
-	p.SetEOSHandler(func() {
-		p.Stop()
-		closeCh <- struct{}{}
-	})
-	p.SetErrorHandler(func(err error) {
-		log.Println(err)
-		p.Stop()
-		closeCh <- struct{}{}
-	})
-	p.Start()
-	log.Println("starting pipeline")
+	fmt.Println("opened stream to server")
 
-	s := moqtransport.Server{
-		Handler: moqtransport.PeerHandlerFunc(func(p *moqtransport.Peer) {
-			log.Println("handling new peer")
-			defer log.Println("XXX")
-			p.OnAnnouncement(func(s string) error {
-				log.Printf("got announcement: %v", s)
-				return nil
-			})
-			if err := p.Announce("video"); err != nil {
-				log.Printf("failed to announce video: %v", err)
+	client_listener, err := quic.ListenAddr(relay_server_address,
+		video_generateTLSConfig(), video_generateQuicConfig())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("listening for clients")
+
+	client_list := make([]Client, 0)
+
+	go passOnVideo(server_conn, server_str, client_list)
+
+	for {
+		c_conn, err := client_listener.Accept(context.Background())
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("accepted client connection")
+
+		go func(c_conn quic.Connection) {
+
+			c_str, err := c_conn.AcceptStream(context.Background())
+			if err != nil {
+				panic(err)
 			}
-			log.Println("announced video namespace")
-			p.OnSubscription(func(s string, st *moqtransport.SendTrack) (uint64, time.Duration, error) {
-				log.Printf("handling subscription to track %s", s)
-				if s != "video" {
-					return 0, 0, errors.New("unknown trackname")
-				}
-				p, err := gst.NewPipeline("appsrc name=videosrc ! queue ! videoconvert ! jpegenc ! multipartmux ! appsink name=appsink")
-				if err != nil {
-					log.Fatal(err)
-				}
+			fmt.Println("accepted client stream")
+			client := Client{
+				conn:   c_conn,
+				stream: c_str,
+			}
+			client_list = append(client_list, client)
 
-				// videosrc, err := p.GetElement("videosrc")
-				// if err != nil {
-				// 	log.Fatal(err)
-				// }
-
-				p.SetBufferHandler(func(b gst.Buffer) {
-					if _, err := st.Write(b.Bytes); err != nil {
+			go func(c_str quic.Stream) {
+				for {
+					buf := make([]byte, 64_000)
+					n, err := c_str.Read(buf)
+					if err != nil {
 						panic(err)
 					}
-				})
-				p.SetEOSHandler(func() {
-					p.Stop()
-				})
-				p.SetErrorHandler(func(err error) {
-					log.Println(err)
-					p.Stop()
-				})
-				p.Start()
-				return 0, 0, nil
-			})
-		}),
-		TLSConfig: video_generateTLSConfig(),
+					// send to server
+					_, err = server_str.Write(buf[:n])
+					if err != nil {
+						panic(err)
+					}
+				}
+			}(c_str)
+		}(c_conn)
+
+		fmt.Println("new client connected")
 	}
-	if err := s.ListenQUIC(context.Background(), relay_server_address); err != nil {
-		return err
-	}
-	return nil
+
 }
 
 func client() error {
@@ -182,7 +168,7 @@ func client() error {
 
 	c, err := moqtransport.DialQUIC(context.Background(), relay_server_address)
 	if err != nil {
-		return err
+		panic(err)
 	}
 
 	log.Println("moq peer connected")
@@ -196,11 +182,11 @@ func client() error {
 	log.Printf("got announcement: %v", trackname)
 	p, err := gst.NewPipeline("appsrc name=src ! multipartdemux ! jpegdec ! autovideosink")
 	if err != nil {
-		return err
+		panic(err)
 	}
 	t, err := c.Subscribe(trackname)
 	if err != nil {
-		return err
+		panic(err)
 	}
 	p.SetEOSHandler(func() {
 		p.Stop()
@@ -240,6 +226,43 @@ func client() error {
 	return nil
 }
 
+func passOnVideo(server_conn quic.Connection, server_str quic.Stream, client_list []Client) {
+
+	// Pass on datagrams from the server to all clients
+	go func(s_c quic.Connection, c_lst []Client) {
+		for {
+			buf, err := s_c.ReceiveDatagram(context.Background())
+			if err != nil {
+				panic(err)
+			}
+			for _, c := range c_lst {
+				err := c.conn.SendDatagram(buf)
+				if err != nil {
+					panic(err)
+				}
+			}
+		}
+	}(server_conn, client_list)
+
+	// Pass on stream data from the server to all clients
+	go func(s_str quic.Stream, c_lst []Client) {
+		for {
+			buf := make([]byte, 64_000)
+			n, err := s_str.Read(buf)
+			if err != nil {
+				panic(err)
+			}
+			for _, c := range c_lst {
+				_, err := c.stream.Write(buf[:n])
+				if err != nil {
+					panic(err)
+				}
+			}
+
+		}
+	}(server_str, client_list)
+}
+
 // Setup a bare-bones TLS config for the server
 func video_generateTLSConfig() *tls.Config {
 	key, err := rsa.GenerateKey(rand.Reader, 1024)
@@ -266,8 +289,15 @@ func video_generateTLSConfig() *tls.Config {
 	}
 
 	return &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-		NextProtos:   []string{"moq-00"},
-		KeyLogWriter: keylogFile,
+		Certificates:       []tls.Certificate{tlsCert},
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"moq-00"},
+		KeyLogWriter:       keylogFile,
+	}
+}
+
+func video_generateQuicConfig() *quic.Config {
+	return &quic.Config{
+		EnableDatagrams: true,
 	}
 }
